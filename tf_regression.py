546eaee27d8901be8d55753636474a47759943b6
MrZeRo2000 <MrZeRo2000@mail.ru>
2018-04-03 17:56:26 +0300

    regression with tf estimator

--- tf_regression.py
+++ tf_regression.py
@@ -44,7 +44,7 @@ with tf.Session() as sess:
 
         rand_ind = np.random.randint(len(x_data), size=batch_size)
 
-        feed = {xph: x_data[rand_ind], yph: [rand_ind]}
+        feed = {xph: x_data[rand_ind], yph: y_true[rand_ind]}
 
         sess.run(train, feed_dict=feed)
 
@@ -56,4 +56,40 @@ y_hat = x_data * model_m + model_b
 my_data.sample(250).plot(kind='scatter', x='X Data', y='Y')
 
 plt.plot(x_data, y_hat, 'red')
+plt.show()
+
+"""TF estimator"""
+
+feat_cols = [tf.feature_column.numeric_column('x', shape=[1])]
+estimator = tf.estimator.LinearRegressor(feature_columns=feat_cols)
+
+from sklearn.model_selection import train_test_split
+x_train, x_eval, y_train, y_eval = train_test_split(x_data, y_true, test_size=0.3, random_state=101)
+
+input_func = tf.estimator.inputs.numpy_input_fn({'x': x_train}, y_train,
+                                                batch_size=8, num_epochs=None, shuffle=True)
+train_input_func = tf.estimator.inputs.numpy_input_fn({'x': x_train}, y_train,
+                                                      batch_size=8, num_epochs=1000, shuffle=False)
+eval_input_func = tf.estimator.inputs.numpy_input_fn({'x': x_eval}, y_eval,
+                                                      batch_size=8, num_epochs=1000, shuffle=False)
+
+estimator.train(input_fn=input_func, steps=1000)
+
+train_metrics = estimator.evaluate(input_fn=train_input_func, steps=1000)
+eval_metrics = estimator.evaluate(input_fn=eval_input_func, steps=1000)
+
+print('Training data metrics')
+print(train_metrics)
+print('Eval metrics')
+print(eval_metrics)
+
+brand_new_data = np.linspace(0, 10, 10)
+predict_input_func = tf.estimator.inputs.numpy_input_fn({'x': brand_new_data}, shuffle=False)
+
+predictions = []
+for pred in estimator.predict(input_fn=predict_input_func):
+    predictions.append(pred['predictions'])
+
+my_data.sample(n=250).plot(kind='scatter', x='X Data', y='Y')
+plt.plot(brand_new_data, predictions, 'r*')
 plt.show()
